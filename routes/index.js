fed01135c25e78fa2bef9a893d2205c7c0ba7792
ohanlines <rauhanhouse7@gmail.com>
2022-07-07 23:58:46 +0700

    simple quiz js ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+let counterBenar = 0;
+let counterSalah = 0;
+let counterSekip = 0;
+
+const soal = {
+  text_soal : "Erwin menjual bakiak bersama Bang Bewok. Erwin merupakan ...." ,
+  options : ["kata benda konkret", "kata benda abstrak", "bukan kata benda"] ,
+  answer : "kata benda konkret",
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Selamat Pagi Dunia' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title:"contoh soal", soal, counterBenar, counterSalah, counterSekip });
+});
+
+/* POST increase counter by 1. */
+router.post("/increase-counter", function (req, res, next) {
+  let c_answer = req.body.soal;
+  console.log(c_answer);
+  if (c_answer === soal.answer){
+    counterBenar += 1;
+  }
+  else if(!c_answer){
+    counterSekip += 1;
+  }
+  else {
+    counterSalah += 1;
+  }
+  res.redirect("/");
 });
 
 module.exports = router;
